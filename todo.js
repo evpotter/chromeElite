1d40faf44a49f9fc19bddfc14ce6a2e91c743136
Austin Feight <afeight@umich.edu>
2014-02-08 16:06:48 -0500

    calendar, showing based on time done

--- todo.js
+++ todo.js
@@ -1,35 +1,12 @@
-angular.module('project', ['ngRoute', 'firebase'])
- 
-.value('fbURL', 'https://angularjs-projects.firebaseio.com/')
- 
-.factory('Projects', function($firebase, fbURL) {
-  return $firebase(new Firebase(fbURL));
-})
- 
-.controller('ListCtrl', function($scope, Projects) {
-  $scope.projects = Projects;
-})
- 
-.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
-  $scope.save = function() {
-    Projects.$add($scope.project, function() {
-      $timeout(function() { $location.path('/'); });
-    });
-  };
-})
- 
-.controller('EditCtrl',
-  function($scope, $location, $routeParams, $firebase, fbURL) {
-    var projectUrl = fbURL + $routeParams.projectId;
-    $scope.project = $firebase(new Firebase(projectUrl));
- 
-    $scope.destroy = function() {
-      $scope.project.$remove();
-      $location.path('/');
-    };
- 
-    $scope.save = function() {
-      $scope.project.$save();
-      $location.path('/');
-    };
-});
+var app = angular.module("chromeElite", ["firebase"])
+
+function TodoWidget($scope, $firebase) {
+  var ref = new Firebase("https://chromeelite.firebaseio.com/")
+  $scope.todos = $firebase(ref)
+  console.log($scope.todos)
+  $scope.addTodo = function(e) {
+    // if (e && e.keyCode != 13) return
+    $scope.todos.$add({text: $scope.formTodoText, done:false})
+    $scope.formTodoText = ""
+  }
+}
