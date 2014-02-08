533e1d55d13d4b289338e1104889c0d6fc051e66
Vinayak Thapliyal <vinayakthapliyal@gmail.com>
2014-02-08 13:18:00 -0500

    Displaying random frontpage r/pics

--- evening.js
+++ evening.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-  
+  $('#background').hide();
+  $('#redditWidget').hide();
   $.get("http://www.reddit.com/r/pics",function(data,status) {
-    var image = $(data).find("a").filter(function() {
-      return $(this).attr('href').match(/.*imgur.*/);
-    }).first().attr('href');
+    var imageDOM = $(data).find("a").filter(function() {
+      return $(this).attr('href').match(/.*jp[g|eg]/);
+    });
+    var length = imageDOM.size();
+    var ran = Math.floor(Math.random() * length);
+    var imageObj = imageDOM.get(ran);
+    var image = $(imageObj).attr('href');
+    var text = $(imageObj).text();
+    //$('#background').fadeIn("slow");
+    //$('#redditWidget').fadeIn("slow");
+    $('#background').show();
+    $('#redditWidget').fadeIn();
     $("#background").css("background-image", "url("+image+")");
-    $("#redditWidget").append('<img src="'+image+'" width="200"/>');
+    $("#redditWidget").append('<h1>'+text+'</h1><br/>');
+    $("#redditWidget").append('<img src="'+image+'">');
   });
       //.find('img src=\.*imgur*.\')
       //.filter(function() {
